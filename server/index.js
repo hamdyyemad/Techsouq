c1097777ce9477e0b481709ca920f3fa6c89c6fe
hamdyyemadd <hamdyyemad@aol.com>
2024-07-07 05:53:44 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -45,7 +45,6 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 require("dotenv").config();
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
